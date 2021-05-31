02b890181c947ff3990ed35c9400733ae2975aaa
Aleksi Kilponen <akkilp@utu.fi>
2021-05-31 11:57:39 +0300

    dynamic server port added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(3050);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3050);
 }
 bootstrap();
