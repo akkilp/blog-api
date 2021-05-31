3f7b3239e55b04c4a0c476cf5cc421227e771111
Aleksi Kilponen <akkilp@utu.fi>
2021-05-31 12:43:47 +0300

    port fix

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(port);
 }
 bootstrap();
