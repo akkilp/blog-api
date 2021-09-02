a804965b8c4884c2b52473d9c4e1199cb642e729
Aleksi Kilponen <akkilp@utu.fi>
2021-09-02 17:51:31 +0300

    pulled

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,8 @@ async function bootstrap() {
     logger: true,
   });
   app.use(cookieParser());
-  app.enableCors({
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? process.env.PROD_CLIENT
-        : 'http://localhost:8080',
-    credentials: true,
-  });
+  app.enableCors({ origin: 'http://localhost:8080', credentials: true });
+
   await app.listen(process.env.PORT as string | 3050);
 }
 bootstrap();
